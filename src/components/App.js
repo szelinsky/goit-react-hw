51a2664fa10c18f240e714faeb8a66db003fd890
Sergey Zelinsky <zelinsky.serega@gmail.com>
2020-02-01 15:46:31 +0200

    Made react projects template on master branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,13 @@
 import React from 'react';
-//import Numbers from './numbers/Numbers';
-import user from '../user.json';
-import Profile from './profile/Profile';
-import statistic from '../statistical-data.json';
-import Statistic from './statistic/Statistic';
 
-
-//console.log(user);
-
-//const arr = [1, 2, 3, 4, 5];
-
-export const App = (props) => {
+const App = () => {
 	return (
 		<>
-			<Profile user={user}/>
-			<Statistic statistic={statistic} />
-
+			Хеллоу Эврибади!
 		</>
 	);
-	
-  // return (
-  //   <>
-	// 		{	console.log('props', props) }      
-	// 		<ul>
-  //       {arr.map((item, idx, arr) => (
-  //         <Numbers key={idx} item={item} idx={idx} arr={arr} />
-  //       ))}
-  //     </ul>
-  //     <div>Hello App!</div>
-  //   </>
-	// );
- 	
-
 };
 
-//export default App;
+export default App;
 
 
-//export default App;
--- src/components/numbers/user/User.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const User = ({item = '0'}) => <span>User{item}</span>;
-
-export default User;
-
-
