9af0ebbb07dc84fc080e06eb7b54e961f9dc1755
Sergey Zelinsky <zelinsky.serega@gmail.com>
2020-02-03 10:00:58 +0200

    Updates

---
+++ precache-manifest.efb90ea95605751e7aca9e2d9756d6e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5f1a189028583cdd6d45406aac79a3",
+    "url": "/goit-react-hw/index.html"
+  },
+  {
+    "revision": "615846cdc1109e9727dc",
+    "url": "/goit-react-hw/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "7a81ece03002d936ec23",
+    "url": "/goit-react-hw/static/css/main.6328601c.chunk.css"
+  },
+  {
+    "revision": "615846cdc1109e9727dc",
+    "url": "/goit-react-hw/static/js/2.00be9155.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/goit-react-hw/static/js/2.00be9155.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a81ece03002d936ec23",
+    "url": "/goit-react-hw/static/js/main.00823ad0.chunk.js"
+  },
+  {
+    "revision": "e1830cb7d3d0a5908a78",
+    "url": "/goit-react-hw/static/js/runtime-main.39e4a69a.js"
+  }
+]);
